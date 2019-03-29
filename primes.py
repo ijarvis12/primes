6d34c84bcd8693ca367e051610ca28b73a26bfe8
Ian Jarvis <ijarvis12@gmail.com>
2019-03-29 16:15:41 -0500

    adding comments

--- primes.py
+++ primes.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+# function primes finds the prime numbers given inputs
+# inputs: numprocs: number of processes running the function
+#                p: process number
+#             maxn: maximum integer to search for primes
 def primes(numprocs,p,maxn):
+    # variable a is the starting number to search for primes
+    # defined as the max integer times process number divided by number of processes
     a = int(float(maxn*p)/float(numprocs))
 
     while a < int(float(maxn*(p+1))/float(numprocs)):
@@ -11,14 +17,18 @@ def primes(numprocs,p,maxn):
         else:
             print(a)
         
+#       increment variable
         a += 1
 
-
+##                                                  ##
+## main process that spawns jobs for finding primes ##
+##                                                  ##
 if __name__ == '__main__':
 
     import multiprocessing
     from time import sleep
 
+#   number of processes the computer has
     numprocs = multiprocessing.cpu_count()
 
     print("")
@@ -27,8 +37,10 @@ if __name__ == '__main__':
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print("")
 
+#   maximum number to search to
     maxn = input("Enter max number to search to: ")
 
+#   attempt to make maxn into integer if exists, else exit
     if len(maxn) > 0:
         try:
             maxn = int(maxn)
@@ -39,6 +51,7 @@ if __name__ == '__main__':
     else:
         exit()
 
+#   check maxn to see if it's sane
     if maxn <= 1:
         print("Bad input")
         garbage = input("Press <Enter> to end program")
@@ -46,12 +59,15 @@ if __name__ == '__main__':
 
     print("The primes:")
 
+#   start multiprocessing jobs
     jobs = []
     for p in range(numprocs):
         job = multiprocessing.Process(target=primes, args=(numprocs,p,maxn,))
         jobs.append(job)
         job.start()
+#       need to sleep main process to let subprocess start before main process spawns another
         sleep(0.1)
+#   wait for jobs to finish
     for job in jobs:
         job.join()
 
--- primes2.py
+++ primes2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+##                                  ##
+## program that finds prime numbers ##
+##                                  ##
 print("")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print(" This program finds prime numbers ")
@@ -7,8 +10,10 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("")
 
 print("Enter max number to search to:")
+# variable maxn is the maximum number that the program with search primes (starts at 2)
 maxn = input()
 
+# check to see if input, attempt to make it into integer, else exit
 if len(maxn) > 0:
     try:
         maxn = int(maxn)
@@ -19,6 +24,7 @@ if len(maxn) > 0:
 else:
     exit()
 
+# check if maxn is sane
 if maxn <= 1:
     print("Bad input")
     garbage = input("Press <Enter> to end program")
@@ -26,6 +32,7 @@ if maxn <= 1:
 
 print("The primes:")
 
+# find primes
 for n in range(2,maxn):
     for p in range(2,n):
         if n%p == 0:
