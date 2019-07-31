44208a845356140a6ead9831f3bce1896722364d
Ian Jarvis <ijarvis12@gmail.com>
2019-07-31 15:03:22 -0500

    Initial commit

---
+++ digits-primes.hs
@@ -0,0 +1,17 @@
+#!/usr/bin/env runhaskell
+
+--program that finds number of digits of Mersenne primes
+
+prime :: Integer -> IO ()
+prime x = do
+    let y = 2^(x)*(2^(x+1)-1)
+    print (length (show y))
+
+main :: IO ()
+main = do
+    putStrLn "Finding Mersenne prime..."
+    putStrLn "Enter power:"
+    i <- getLine
+    putStrLn " "
+    let x = read i :: Integer
+    prime x
