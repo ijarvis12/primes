f93c88190a14f8132032566e60ce2996e4578f6f
Ian Jarvis <ijarvis12@gmail.com>
2021-12-19 10:37:14 -0600

    fixed w var in prime func

--- primes2.hs
+++ primes2.hs
@@ -17,7 +17,7 @@ innrprime n x =
 prime :: Integer -> [[Bool]]
 prime x = do
     let z = 2 :: Integer
-    let w = (sqrt x + 1) :: Integer
+    let w = floor ((sqrt x) + 1/2)
     forM [z..w] $ \n -> innrprime n x
 
 loop :: Integer -> Integer -> IO ()
