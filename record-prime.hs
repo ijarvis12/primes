935863d13f0cced38fcc1469e3e771af44ee2050
Ian Jarvis <ijarvis12@gmail.com>
2021-12-19 19:09:30 -0600

    2nd w var fix

--- record-prime.hs
+++ record-prime.hs
@@ -23,7 +23,7 @@ startprimes y = do
     --starting value to modulo on
     let z = 2 :: Integer
     --upper limit of modulo need to check
-    let w = floor ((sqrt y) + 1/2)
+    let w = round (sqrt y) :: Integer
     --do the work of finding the prime
     forM [z..w] $ \n -> primework n y
 
