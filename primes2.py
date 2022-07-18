a3f9d3c4ffbf7570a54a3c50f676f40e0828309a
Ian Jarvis <ijarvis12@gmail.com>
2022-07-18 13:08:15 -0500

    Sieve algo

--- primes2.py
+++ primes2.py
@@ -36,11 +36,24 @@ if maxn <= 1:
 print("The primes:")
 
 # find primes
-for n in range(2,maxn):
-    for p in range(2,int(sqrt(n))+1):
-        if n % p == 0:
-            break
-    else:
-        print(n)
+#for n in range(2,maxn):
+#    for p in range(2,int(sqrt(n))+1):
+#        if n % p == 0:
+#            break
+#    else:
+#        print(n)
+
+A = [True,True]
+for i in range(2,maxn+1):
+    A.append(True)
+for i in range(2,int(sqrt(maxn))+1):
+    if A[i]:
+        for j in range(i*i,maxn+1,i):
+            A[j] = False
+
+for i,a in enumerate(A):
+    if a:
+        print(i)
+
 
 _ = input("Press <Enter> to end program")
