cec0331beb727de1c701c7ccb27485efed6b97f2
Ian Jarvis <ijarvis12@gmail.com>
2022-08-21 09:01:48 -0500

    undo commit

--- primes2.py
+++ primes2.py
@@ -1,85 +1,46 @@
 #!/usr/bin/env python3
 
-##                                     ##
-## program that finds perfect numbers  ##
-##                                     ##
+##                                  ##
+## program that finds prime numbers ##
+##                                  ##
 
 from math import sqrt
 
 print("")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print(" This program finds perfect numbers using Mersenne Primes ")
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(" This program finds prime numbers ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("")
 
-# Lucas-Lehmer prime test for odd p > 2
-def LLT(p):
-    s = 4
-    M = 2**p - 1
-    for n in range(0,p-2):
-        s = ((s * s) - 2) % M;
-        if(s == 0):
-            return False
-    return True
+print("Enter max number to search to:")
+# variable maxn is the maximum number that the program with search primes (starts at 2)
+maxn = input()
 
-print("The perfect numbers:")
-print(6)
+# check to see if input, attempt to make it into integer, else exit
+if len(maxn) > 0:
+    try:
+        maxn = int(maxn)
+    except:
+        print("Bad input")
+        _ = input("Press <Enter> to end program")
+        exit()
+else:
+    exit()
 
-# find the perfect numbers
-p = 1
-while True:
-    p += 2
-    psum = 0
+# check if maxn is sane
+if maxn <= 1:
+    print("Bad input")
+    _ = input("Press <Enter> to end program")
+    exit()
 
-#   LLT check
-    if LLT(p):
-        continue
+print("The primes:")
 
-#   the potential perfect number
-    perfect = 2**(p-1)*(2**(p)-1)
+# find primes
+for n in range(2,maxn):
+    for p in range(2,int(sqrt(n))+1):
+        if n % p == 0:
+            break
+    else:
+        print(n)
 
-#   the limit to search to
-    sqrtp = int(sqrt(perfect))
-
-#   Sieve for primes
-    A = [False,True]
-    for i in range(2,sqrtp+1):
-        A.append(True)
-    for i in range(2,int(sqrt(sqrtp))+1):
-        if A[i]:
-            for j in range(i*i,sqrtp+1,i):
-                A[j] = False
-    primes = []
-    for i,a in enumerate(A):
-        if a:
-            primes.append(i)
-
-#   add up all the divisors into psum
-#    for n in range(1,sqrtp+1):
-#        if perfect % n == 0:
-#            psum += n
-#            psum += perfect // n
-
-    for pri in primes:
-        if perfect % pri == 0:
-            psum += pri
-            psum += perfect // pri
-        elif pri != primes[-1]:
-            n = pri
-            stop = primes[primes.index(pri)+1]
-            while True:
-                if n >= stop:
-                    break
-                n += 1
-                if perfect % n == 0:
-                    psum += n
-                    psum += perfect // n
-                    break
-
-#   get rid of possible extra summation
-    if sqrtp**2 == perfect:
-        psum -= sqrtp
-
-#   if psum is equal to the potenial perfect number, we have a match
-    if psum == 2*perfect:
-        print(perfect)
+_ = input("Press <Enter> to end program")
