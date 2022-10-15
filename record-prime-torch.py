9c44b442264cfed99716377e1fa6d4cd34fad379
Ian Jarvis <ijarvis12@gmail.com>
2022-10-15 15:52:06 -0500

    initial commit

---
+++ record-prime-torch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+print("")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(" This program finds prime numbers ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("")
+
+import torch
+from math import sqrt
+
+cpu = torch.device('cpu')
+cuda = torch.device('cuda')
+
+print("The primes:")
+print(2)
+
+# starting Mersenne prime power
+x = 1
+
+# start loop to find primes
+while x<1000:
+	
+#   iterate Mersenne prime power
+	x += 1
+	
+	with torch.cuda.device()
+
+#       Mersenne prime
+		n = torch.tensor(2**x - 1).cuda()
+		bl = torch.tensor(0, device=cuda)
+	
+#       do the grunt work
+		for i in range(start,int(sqrt(n))+1):
+			if n % i == 0:
+				bl = 1
+				break
+		
+#       send vars to cpu for displaying
+		bl.to(device=cpu)
+		n.to(device=cpu)
+		
+#   print number if prime        
+	if bl.item() == 1:
+		pass
+	else:
+		print(n.item())
+
+print()
+_ = input('Press <Enter> to end program.')
