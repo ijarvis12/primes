f58b6cf7adba56d061e6584bcbe333389ee33401
Ian Jarvis <ijarvis12@gmail.com>
2022-11-12 15:12:52 -0600

    made more concise

--- primes.hs
+++ primes.hs
@@ -2,35 +2,25 @@
 
 --program that finds prime numbers
 
-innrprime :: Integer -> Integer -> IO ()
-innrprime z x =
-    if (z == (div x 2)) then
-        print x
-    else
-        if ((x `mod` z) == 0) then
-            return ()
-        else
-            innrprime (z+1) x
+check :: Integer -> Integer -> Bool
+check n x = mod x n == 0
 
-primes :: Integer -> IO ()
-primes x = do
-    let z = 2 :: Integer
-    innrprime z x
+primes :: Integer -> Integer -> Integer -> Bool -> IO ()
+primes n x 0 False = print x
+primes n x end False = primes (n+1) x (end-1) (check n x)
+primes n x end True = return ()
 
-loop :: Integer -> Integer -> IO ()
-loop x y =
-    if (x > y) then
-        return ()
-    else do
-        primes x
-        loop (x+1) y
+loop :: Integer -> Integer -> Integer -> IO ()
+loop x y 0 = return ()
+loop x y end = do
+    primes 2 x (ceiling (sqrt (fromIntegral x :: Float))) False
+    loop (x+1) y (end-1)
 
 main :: IO ()
 main = do
     putStrLn "Finding primes..."
     putStrLn "Enter max number to search to:"
     i <- getLine
-    putStrLn "\n2"
+    putStrLn "\n2\n3"
     let y = read i :: Integer
-    let x = 2 :: Integer
-    loop x y
+    loop 4 y y
