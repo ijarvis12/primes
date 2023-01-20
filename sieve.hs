b5765ca4c12881fd77c6a78a9eba8d11d8ab8f7f
Ian Jarvis <ijarvis12@gmail.com>
2023-01-19 22:04:02 -0600

    initial commit

---
+++ sieve.hs
@@ -0,0 +1,36 @@
+#!/usr/bin/env runhaskell
+
+--program that finds prime numbers
+
+import Control.Monad
+
+check :: Integer -> Integer -> Bool
+check x n = (mod x n) == 0
+
+check2 :: Integer -> [Integer] -> Bool -> [Integer]
+check2 x lst False = lst
+check2 x lst True = lst ++ [x]
+
+sieve :: Integer -> Integer -> Integer -> [Integer] -> Bool -> Bool
+sieve _ 0 _ [] False = True
+sieve _ _ _ [] True = False
+sieve x y z [] False = sieve x (y-1) (z-1) [] (check x z)
+sieve _ _ _ lst True = False
+sieve x y z lst False = sieve x y z (tail lst) (check x (head lst))
+
+loop :: Integer -> Integer -> [Integer]-> IO ()
+loop x 3 lst = return ()
+loop x end lst = do
+    let z = toInteger (ceiling (sqrt (fromIntegral x)))
+    let b = sieve x (mod (last lst) z) z lst False
+    when b $ print x
+    loop (x+1) (end-1) (check2 x lst b)
+
+main :: IO ()
+main = do
+    putStrLn "Finding primes..."
+    putStrLn "Enter max number to search to:"
+    i <- getLine
+    putStrLn "\n2\n3"
+    let y = read i :: Integer
+    loop 4 y [2,3]
