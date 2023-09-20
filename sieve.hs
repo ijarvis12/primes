54747be4952797f11f1a03317eb81a2fdee3fe66
Ian Jarvis <ijarvis12@gmail.com>
2023-09-20 17:05:43 -0500

    Update sieve.hs
    algorithm change, a bit faster

--- sieve.hs
+++ sieve.hs
@@ -13,19 +13,19 @@ additem :: Integer -> [Integer] -> Bool -> [Integer]
 additem _ lst False = lst
 additem x lst True = lst ++ [x]
 
-sieve :: Integer -> Integer -> Integer -> [Integer] -> Bool -> Bool
-sieve _ 0 _ [] False = True
-sieve _ _ _ _ True = False
-sieve x y z [] False = sieve x (y-1) (z-1) [] (check x z)
-sieve x y z lst False = sieve x y z (tail lst) (check x (head lst))
+sieve :: Integer -> [Integer] -> Bool -> Bool
+sieve _ _ True = False
+sieve _ [] False = True
+sieve x lst False = sieve x (tail lst) (check x (head lst))
 
 loop :: Integer -> Integer -> [Integer]-> IO ()
 loop _ 3 _ = return ()
 loop x end lst = do
     let z = toInteger (ceiling (sqrt (fromIntegral x)))
-    let b = sieve x (mod (last lst) z) z lst False
-    prnt x b
-    loop (x+1) (end-1) (additem x lst b)
+    let b = sieve x lst False
+    let c = null [y | y <- [(last lst)..z], mod x y == 0 ]
+    prnt x (b&&c)
+    loop (x+2) (end-2) (additem x lst (b&&c))
 
 main :: IO ()
 main = do
@@ -33,5 +33,5 @@ main = do
     putStrLn "Enter max number to search to:"
     i <- getLine
     putStrLn "\n2\n3"
-    let y = read i :: Integer
-    loop 4 y [2,3]
+    let z = read i :: Integer
+    loop 5 z [2,3]
