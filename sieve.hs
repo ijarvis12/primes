e1b35bfd10071a894b85b7e6586d94476c0af65d
Ian Jarvis <ijarvis12@gmail.com>
2024-02-13 11:41:26 -0600

    Update sieve.hs

--- sieve.hs
+++ sieve.hs
@@ -6,18 +6,21 @@
 check :: Integer -> Integer -> Bool
 check x n = (mod x n) == 0
 
+check2 :: Integer -> Integer -> Bool
+check2 x n = (toIntegral (ceiling (sqrt n)) :: Integer) < x
+
 additem :: Integer -> [Integer] -> Bool -> [Integer]
 additem _ lst False = lst
 additem x lst True = lst ++ [x]
 
-sieve :: Integer -> [Integer] -> Bool -> Bool
-sieve _ _ True = False
-sieve _ [] False = True
-sieve x lst False = sieve x (tail lst) (check x (head lst))
+sieve :: Integer -> [Integer] -> Bool -> Bool -> Bool
+sieve _ _ True _ = False
+sieve _ _ False True = True
+sieve x [l:ls] False False = sieve x ls (check x l) (check2 x l)
 
 loop :: Integer -> Integer -> [Integer]-> IO ()
 loop _ 3 lst = print lst
-loop x end lst = loop (x+1) (end-1) (additem x lst (sieve x lst False))
+loop x end lst = loop (x+1) (end-1) (additem x lst (sieve x lst False False))
 
 main :: IO ()
 main = do
