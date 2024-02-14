e4b17845da7c0f57aacaf4df3c9cdbfb93a39b99
Ian Jarvis <ijarvis12@gmail.com>
2024-02-14 10:32:28 -0600

    Update sieve.hs

--- sieve.hs
+++ sieve.hs
@@ -3,24 +3,18 @@
 --program that finds prime numbers
 --using the Sieve of Eratosthenes
 
-check :: Integer -> Integer -> Bool
-check x n = (mod x n) == 0
-
-check2 :: Integer -> Integer -> Bool
-check2 x n = (toIntegral (ceiling (sqrt n)) :: Integer) < x
-
 additem :: Integer -> [Integer] -> Bool -> [Integer]
 additem _ lst False = lst
 additem x lst True = lst ++ [x]
 
-sieve :: Integer -> [Integer] -> Bool -> Bool -> Bool
-sieve _ _ True _ = False
-sieve _ _ False True = True
-sieve x [l:ls] False False = sieve x ls (check x l) (check2 x l)
+sieve :: Integer -> Integer -> [Integer] -> Bool -> Bool -> Bool
+sieve _ _ _ True _ = False
+sieve _ _ _ False False = True
+sieve x limit (l:ls) False True = sieve x limit ls ((mod x l)==0) (l<limit)
 
-loop :: Integer -> Integer -> [Integer]-> IO ()
-loop _ 3 lst = print lst
-loop x end lst = loop (x+1) (end-1) (additem x lst (sieve x lst False False))
+loop :: Integer -> Integer -> [Integer] -> IO ()
+loop _ 7 lst = print lst
+loop x end lst = loop (x+1) (end-1) (additem x lst (sieve x ((toInteger (ceiling (sqrt (fromIntegral x)))) + 1) lst False True))
 
 main :: IO ()
 main = do
@@ -28,4 +22,4 @@ main = do
     putStrLn "Enter max number to search to:"
     i <- getLine
     let z = read i :: Integer
-    loop 4 (max (abs z) 4) [2,3]
+    loop 8 (max (abs z) 8) [2,3,5,7]
